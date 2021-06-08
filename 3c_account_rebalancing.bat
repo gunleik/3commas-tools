5cfd2c2e9bce107f926cf9ec354bd66e96dfa28c
gunleik <agunleik@gmail.com>
2021-06-09 00:20:03 +0200

    changed to only use one 3c_key file

--- 3c_account_rebalancing.bat
+++ 3c_account_rebalancing.bat
@@ -1,24 +1,24 @@
 @echo off
 
-if exist 3c_rw_keys.bat (
-	3c_rw_keys.bat
+if exist 3c_keys.bat (
+	3c_keys.bat
 	python 3c_account_rebalancing.py %1 %2
 	pause
 ) else (
 	echo If not done already, you have to create an API key at https://3commas.io/api_access_tokens
 	echo It needs minimum "Account Read", "SmartTradesRead" and "SmartTradesWrite" access
 	echo.
-    echo No existing 3c_rw_keys.bat file, fill inn the API info here:
+    echo No existing 3c_keys.bat file, fill inn the API info here:
 
-	set /p threecommasapirwkey=3commas API key: 
-	set /p threecommasapirwsecret=3commas API secret: 
+	set /p threecommasapikey=3commas API key: 
+	set /p threecommasapisecret=3commas API secret: 
 )
 
-if not exist 3c_rw_keys.bat (
-	echo set threecommas_rw_key=%threecommasapirwkey% >3c_rw_keys.bat
-	echo set threecommas_rw_secret=%threecommasapirwsecret% >>3c_rw_keys.bat
+if not exist 3c_keys.bat (
+	echo set threecommas_key=%threecommasapikey% >3c_keys.bat
+	echo set threecommas_secret=%threecommasapisecret% >>3c_keys.bat
 
-	3c_rw_keys.bat
+	3c_keys.bat
 	python 3c_account_rebalancing.py %1 %2
 	pause
 )
--- 3c_use_account.sh
+++ 3c_use_account.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -f "$HOME/.3c_rw_keys" ]; then
-	source $HOME/.3c_rw_keys
+if [ -f "$HOME/.3c_keys" ]; then
+	source $HOME/.3c_keys
 	python3 3c_use_account.py $1 $2
 else
 	echo "If not done already, you have to create an API key at https://3commas.io/api_access_tokens"
 	echo "It needs minimum \"AccountWrite\" access"
 	echo
-    echo "No existing $HOME/.3c_rw_keys file, fill inn the API info here:"
+    echo "No existing $HOME/.3c_keys file, fill inn the API info here:"
 
 	echo -n "3commas API key: "
 	read threecommasapirwkey
@@ -15,12 +15,12 @@ else
 	echo -n "3commas API secret: "
 	read threecommasapirwsecret
 
-	touch $HOME/.3c_rw_keys
-	chmod 600 $HOME/.3c_rw_keys
+	touch $HOME/.3c_keys
+	chmod 600 $HOME/.3c_keys
 
-	echo "export threecommas_rw_key=\"$threecommasapirwkey\"" >$HOME/.3c_rw_keys
-	echo "export threecommas_rw_secret=\"$threecommasapirwsecret\"" >>$HOME/.3c_rw_keys
+	echo "export threecommas_key=\"$threecommasapirwkey\"" >$HOME/.3c_keys
+	echo "export threecommas_secret=\"$threecommasapirwsecret\"" >>$HOME/.3c_keys
 
-	source $HOME/.3c_rw_keys
+	source $HOME/.3c_keys
 	python3 3c_use_account.py $1 $2
 fi
